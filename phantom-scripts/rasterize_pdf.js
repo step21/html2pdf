747bcb7db9928adbd3ae18bb129ce2e6bdd4c83e
step21 <step21@devtal.de>
2016-10-27 18:54:10 +0200

    merge

--- phantom-scripts/rasterize_pdf.js
+++ phantom-scripts/rasterize_pdf.js
@@ -1,18 +1,19 @@
+var system = require('system');
 var page = new WebPage(),
     address, output, size;
  
-if (phantom.args.length < 2 || phantom.args.length > 3) {
+if (system.args.length < 2 || system.args.length > 3) {
     console.log('Usage: rasterize.js URL filename');
     phantom.exit();
 } else {
-    address = phantom.args[0];
-    output = phantom.args[1];
-		
-		page.paperSize = {
-			format: 'A4',
-			orientation: 'portrait',
-			border: '1.5cm'
-		};
+    address = system.args[1];
+    output = system.args[2];
+
+	page.paperSize = {
+		format: 'A4',
+		orientation: 'portrait',
+		border: '1.5cm'
+	};
 
     page.onConsoleMessage = function(msg) { console.log(msg); };
     page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.0; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.75 Safari/535.7';
